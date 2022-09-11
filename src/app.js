7323f000b5a3e35408231a12bd36dd7252322882
martinamineo98 <martinamineo98@gmail.com>
2022-09-11 05:42:30 +0200

    Updated app.js & app.test.js

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ import {dom} from './app.dom.js'
 export function Ship (length) {
 	return {
 		length: length,
-		status: new Array(length).fill(0),
-		direction: 0, // DEFAULT: horizontal.
+		status: new Array(length).fill(0),  // DEFAULT: not hit.
+		direction: 1, // DEFAULT: horizontal.
+		position: {start: null, end: null},
 		
 		hit (position) {
 			this.status[position] = 1
@@ -18,21 +19,64 @@ export function Ship (length) {
 		
 		isSunk () {
 			return this.status.every((position) => position == 1)
+		},
+		
+		changeDirection (direction) {
+			this.direction = (direction) ? 1 : 0
+		},
+		
+		addPosition (start, end) {
+			this.position.start = start
+			this.position.end = end
 		}
 	}
 }
 
 export function Gameboard (squares) {
 	return {
-		grid: new Array(squares).fill(new Array(squares).fill([])),
-		attacks: {
-			attacked: new Array(squares).fill(new Array(squares).fill(0)),
-			successful: new Array(squares).fill(new Array(squares).fill(0)),
-			unsuccessful: new Array(squares).fill(new Array(squares).fill(0))
+		ships: [],
+		grid: new Array(squares).fill(new Array(squares).fill(null)), // null gets replaced with a ship when placed.
+		attacks: new Array(squares).fill(new Array(squares).fill(null)), // null gets replaced with 1 (successful attack) and 0 (missed attack) when an attack occurs.
+		
+		placeShip (ship, row, column) {	
+			let start = [row, column]
+			let end = []
+			
+			if (this.grid[row][column]) {
+				throw new Error('The choosen square is not empty.')
+			} else {
+				for (let i = 0; i < ship.length; i++) {		
+					end[0] = (Boolean(ship.direction)) ? row : row + i
+					end[1] = (Boolean(ship.direction)) ? column + i : column
+					this.grid[end[0]][end[1]] = ship
+				}
+				
+				ship.addPosition(start, end)
+				this.ships.push(ship)
+			}
 		},
 		
-		placeShip (row, column, direction = true) {
+		attackShip (row, column) {
+			const ship = this.grid[row][column]
 			
+			if (ship) {
+				const position = ship.position.end[(Boolean(ship.direction)) ? 1 : 0] - (Boolean(ship.direction)) ? column : row
+				ship.hit(position)
+			}
+			
+			if (checkIfShipIsSunk(ship)) {
+				// TODO: do something when the ship is sunk.
+			}
+			
+			this.attacks[row][column] = (ship) ? 1 : 0
+		},
+		
+		checkIfShipsAreAllSunk () {
+			return ships.every((ship) => ship.isSunk == true)
+		},
+		
+		checkIfShipIsSunk (ship) {
+			return ship.status.every((position) => Boolean(position))
 		}
 	}
 }
--- src/app.test.js
+++ src/app.test.js
@@ -14,5 +14,36 @@ function TEST_shipIsNotSunk () {
 	return ship.isSunk()
 }
 
-test('The Ship was Sunk', () => expect(TEST_shipIsSunk()).toBe(true))
-test('The Ship was not Sunk', () => expect(TEST_shipIsNotSunk()).toBe(false))
+function TEST_changeShipDirectionToHorizontal () {
+	let ship = new Ship(5)
+	ship.changeDirection(true)
+	return ship.direction
+}
+
+function TEST_changeShipDirectionToVertical () {
+	let ship = new Ship(5)
+	ship.changeDirection(false)
+	return ship.direction
+}
+
+function TEST_placeShip () {
+	let gameboard = new Gameboard(10)
+	let ship = new Ship(5)
+	// gameboard.placeShip(ship, 0, 0)
+	console.log(gameboard.grid)
+	return ship
+}
+
+console.log(TEST_placeShip())
+
+describe('Sunking Ships', () => {
+	test('The Ship was Sunk', () => expect(TEST_shipIsSunk()).toBe(true))
+	test('The Ship was not Sunk', () => expect(TEST_shipIsNotSunk()).toBe(false))	
+})
+
+describe('Changing Directions', () => {
+	test('The Ship is now horizontal', () => expect(TEST_changeShipDirectionToHorizontal()).toBe(1))
+	test('The Ship is now vertical', () => expect(TEST_changeShipDirectionToVertical()).toBe(0))	
+})
+
+// test('Place Ship', () => expect(TEST_placeShip()).toBe('object'))
