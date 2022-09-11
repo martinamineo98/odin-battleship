a74364282162a58b9eba9f40f84c80d09e31801e
martinamineo98 <martinamineo98@gmail.com>
2022-09-11 04:10:12 +0200

    Added isSunk, wasHit, hit capabilities

--- src/app.js
+++ src/app.js
@@ -1,14 +1,45 @@
 
+
 import {dom} from './app.dom.js'
 
 export function Ship (length) {
-	
+	return {
+		length: length,
+		status: new Array(length).fill(0),
+		direction: 0, // DEFAULT: horizontal.
+		
+		hit (position) {
+			this.status[position] = 1
+		},
+		
+		wasHit (position) {
+			return this.status[position] == 1
+		},
+		
+		isSunk () {
+			return this.status.every((position) => position == 1)
+		}
+	}
 }
 
-export function Gameboard (squares = 10) {
-	
+export function Gameboard (squares) {
+	return {
+		grid: new Array(squares).fill(new Array(squares).fill([])),
+		attacks: {
+			attacked: new Array(squares).fill(new Array(squares).fill(0)),
+			successful: new Array(squares).fill(new Array(squares).fill(0)),
+			unsuccessful: new Array(squares).fill(new Array(squares).fill(0))
+		},
+		
+		placeShip (row, column, direction = true) {
+			
+		}
+	}
 }
 
 export function Player () {
-	
+	return {
+		
+	}
 }
+
--- src/app.test.js
+++ src/app.test.js
@@ -3,6 +3,16 @@ import {Ship, Gameboard, Player} from './app.js'
 
 test('TODO', () => expect().toBe())
 
-// test('Return Grid', () => {
-	// expect(returnGrid(new Gameboard(10))).toBe('object')
-// })
+function TEST_shipIsSunk () {
+	let ship = new Ship(5)
+	ship.status.forEach((x, index) => ship.hit(index))
+	return ship.isSunk()
+}
+
+function TEST_shipIsNotSunk () {
+	let ship = new Ship(5)
+	return ship.isSunk()
+}
+
+test('The Ship was Sunk', () => expect(TEST_shipIsSunk()).toBe(true))
+test('The Ship was not Sunk', () => expect(TEST_shipIsNotSunk()).toBe(false))
