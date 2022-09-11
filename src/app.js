286512c0a1b30eb36e56b9eaebe89f92a737ebfe
martinamineo98 <martinamineo98@gmail.com>
2022-09-11 03:11:59 +0200

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -2,20 +2,11 @@
 import {dom} from './app.dom.js'
 
 export function Ship (length) {
-	const ship = {}
-	ship.length = length
-	ship.status = new Array(length).fill(0)
-	ship.hit = (position) => ship.status[position] = 1
-	ship.sunk = () => ship.status.every((position) => position === 1)
-	return ship
+	
 }
 
-// Create a gameboard with number of squares for row.
-
 export function Gameboard (squares = 10) {
-	const gameboard = {}
-	gameboard.grid = new Array(squares).fill(new Array(squares).fill([]))
-	return gameboard
+	
 }
 
 export function Player () {
