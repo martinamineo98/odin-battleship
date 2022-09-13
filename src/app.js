fdc1c6a88b32075b5b20e2350264f5d0cd6886bf
martinamineo98 <martinamineo98@gmail.com>
2022-09-13 08:31:10 +0200

    Updated app.js && app.test

--- src/app.js
+++ src/app.js
@@ -26,6 +26,10 @@ export function Gameboard () {
 	return {
 		grid: new Array(10).fill([]).map(() => [...Array(10).keys()].map(() => null)),
 		attacks: new Array(10).fill([]).map(() => [...Array(10).keys()].map(() => null)),
+		ships: [],
+		checkIfAllShipsAreSunk () {
+			return this.ships.every((ship) => ship.isSunk())
+		},
 		placeShip (ship, row, col, direction) {
 			let squares = []
 			let gridSquares = []
@@ -42,6 +46,7 @@ export function Gameboard () {
 			} else {
 				ship.changeDirection(direction)
 				ship.occupiedSquares = squares
+				this.ships.push(ship)
 				
 				for (let i = 0; i < squares.length; i++) {
 					let newRow = (direction) ? row : row + i
@@ -51,16 +56,19 @@ export function Gameboard () {
 				}
 			}
 		},
-		attack (row, col) {
+		receiveAttack (row, col) {
 			let ship = this.grid[row][col]
 			this.attacks[row][col] = (ship) ? 1 : 0
 			
 			if (ship) {
 				ship.hit(row, col)
+				if (ship.isSunk()) {
+					// TODO: if the ship is sunk do something.
+				}
 			}
 			
-			if (ship.isSunk()) {
-				// TODO: if the ship was sunk do something.
+			if (this.checkIfAllShipsAreSunk()) {
+				// TODO: if all ships are sunk do something.
 			}
 		}
 	}
@@ -68,6 +76,35 @@ export function Gameboard () {
 
 export function Player () {
 	return {
+		attack (gameboard, row, col) {
+			gameboard.receiveAttack(row, col)
+		}
+	}
+}
+
+export function Computer () {	
+	function randomNumber () {
+		return Math.floor(Math.random() * 10)
+	}
+	
+	function randomMove (gameboard) {
+		let row = randomNumber()
+		let col = randomNumber()
 		
+		if (gameboard.attacks[row][col] !== null) {
+			randomMove(gameboard)
+		} else {
+			return [row, col]
+		}
+	}
+
+	return {
+		...Player,
+		attack (gameboard) {
+			let [row, col] = randomMove(gameboard)
+			console.log(gameboard.attacks)
+			gameboard.receiveAttack(row, col)
+			console.log(gameboard.attacks)
+		}
 	}
 }
--- src/app.test.js
+++ src/app.test.js
@@ -1,15 +1,18 @@
 
-import {Ship, Gameboard, Player} from './app.js'
+import {Ship, Gameboard, Player, Computer} from './app.js'
 
 const gameboard = new Gameboard()
 const ship = new Ship(5)
 
 gameboard.placeShip(ship, 0, 5, true)
-gameboard.attack(0, 5)
-gameboard.attack(0, 6)
-gameboard.attack(0, 7)
-gameboard.attack(0, 8)
-gameboard.attack(0, 9)
+// gameboard.receiveAttack(0, 5)
+// gameboard.receiveAttack(0, 6)
+// gameboard.receiveAttack(0, 7)
+// gameboard.receiveAttack(0, 8)
+// gameboard.receiveAttack(0, 9)
+
+const computer = new Computer()
+computer.attack(gameboard)
 
 
 test('TODO', () => expect().toBe())
