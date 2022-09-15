589f2b6c821a658d6d537994de81884a65646430
martinamineo98 <martinamineo98@gmail.com>
2022-09-15 02:16:35 +0200

    Updated app.logic.js & app.test.js

--- src/app.logic.js
+++ src/app.logic.js
@@ -1,1 +1,69 @@
 
+export function Ship (length) {
+	function getIndex (array, element) {
+		return array.map((x) => JSON.stringify(x)).indexOf(JSON.stringify(element))
+	}
+	
+	return {
+		length: length,
+		direction: null,
+		status: [...Array(length).keys()].map((element) => element = 0),
+		squares: [...Array(length).keys()].map((element) => element = null),
+		hit (row, col) {
+			const pos = getIndex(squares, [row, col])
+			if (this.status[pos]) throw new Error('The square was already attacked.')
+			this.status[pos] = 1
+		},
+		isSunk () {
+			return this.status.every((element) => element === 1)
+		},
+		addDirection (direction) {
+			this.direction = (direction) ? 1 : 0
+		},
+		addSquare (index, row, col) {
+			this.squares[index] = [row, col]
+		}
+	}
+}
+
+export function Gameboard () {
+	
+	return {
+		grid: [...Array(10).keys()].map((element) => element = [...Array(10).keys()].map((element) => element = null)),
+		hits: [...Array(10).keys()].map((element) => element = [...Array(10).keys()].map((element) => element = null)),
+		ships: [],
+		placeShip (ship, row, col, direction) {
+			const squares = []
+			const coordinates = []
+			
+			for (let i = 0; i < ship.length; i++) {
+				let _row = (direction) ? row : row + i
+				let _col = (direction) ? col + i : col
+				squares.push(this.grid[_row, _col])
+				coordinates.push([_row, _col])
+			}
+			
+			if (!squares.every((square) => square === null)) {
+				throw new Error('At least one square is not empty.')
+			}
+			
+			ship.addDirection(direction)
+			this.ships.push(ship)
+			
+			for (let i = 0; i < coordinates.length; i++) {
+				let [_row, _col] = coordinates[i]
+				ship.addSquare(i, _row, _col)
+			}
+		},
+		receiveAttack (row, col) {
+			let ship = this.grid[row][col]
+			this.hits[row][col] = (ship) ? 1 : 0
+			if (ship) ship.hit(row, col)
+		},
+		checkIfAllSunk () {
+			let status = []
+			this.ships.forEach((ship) => status.push(ship.isSunk()))
+			return status.every((ship) => ship === true)
+		}
+	}
+}
--- src/app.test.js
+++ src/app.test.js
@@ -1,4 +1,31 @@
 
-import {Ship, Gameboard, Player, Computer, game} from './app.logic.js'
+import {Ship, Gameboard} from './app.logic.js'
 
-test('TODO', () => expect().toBe())
+describe('Ship', () => {
+	
+	let ship = new Ship(5)
+	
+	function returnShipStatus () {
+		return ship.status
+	}
+	
+	function returnShipSquares () {
+		ship.squares[0] = [0, 0]
+		return ship.squares
+	}
+
+	function returnIsSunkTrue () {
+		ship.status = ship.status.map((element) => element = 1)
+		return ship.isSunk()
+	}
+	function returnIsSunkFalse () {
+		ship.status = ship.status.map((element) => element = 0)
+		return ship.isSunk()
+	}
+
+	test('Return Ship Status', () => expect(returnShipStatus()).toStrictEqual([0, 0, 0, 0, 0]))
+	test('Return Ship Squares', () => expect(returnShipSquares()).toStrictEqual([[0, 0], null, null, null, null]))
+	test('Return Sunk Status (True)', () => expect(returnIsSunkTrue()).toBe(true))
+	test('Return Sunk Status (False)', () => expect(returnIsSunkFalse()).toBe(false))
+
+})
